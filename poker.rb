db37a23b5ef72e868f346f27b2917964859f6873
Hemal <h.n.varambhia@gmail.com>
2017-01-11 14:49:01 +0000

    Given a hand with a high of 8 and another hand with a high of queen, the high of queen hand is the best.

--- poker.rb
+++ poker.rb
@@ -1,11 +1,21 @@
 class Poker
+  attr_reader :hands
   def initialize hands
-    
+    @hands = hands
   end
 
   def best_hand
-    [ %w{4S 5S 7H 8D JC} ]
+    [ hands.max_by { |hand| rank hand } ]
   end
+  
+  private
+
+  def rank hand
+    ranks = hand.map { |card| card[/[\d+JQKA]/] }
+    ranks.map { |card| RANKS.index card }.max
+  end
+
+  RANKS = %w{2 3 4 5 6 7 8 9 10 J Q K A} 
 end
 
 module BookKeeping
--- poker_test.rb
+++ poker_test.rb
@@ -11,7 +11,6 @@ class PokerTest < Minitest::Test
   end
 
   def test_highest_card
-    skip
     high_of_8 = %w(4S 5H 6S 8D 2H)
     high_of_queen = %w(2S 4H 6C 9D QH)
     game = Poker.new([high_of_8, high_of_queen])
