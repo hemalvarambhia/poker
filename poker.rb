d0d54f1866594ca7ceafb2abe19375ab2f2d994c
Hemal <h.n.varambhia@gmail.com>
2017-01-11 15:15:20 +0000

    Refactor - removed an unused method, improved code formatting.

--- poker.rb
+++ poker.rb
@@ -1,5 +1,6 @@
 class Poker
   attr_reader :hands
+  
   def initialize hands
     @hands = hands.map { |hand| Hand.new hand }
   end
@@ -30,10 +31,6 @@ class Poker
     end
 
     RANKS = %w{2 3 4 5 6 7 8 9 10 J Q K A} 
-  end
-
-  def rank hand
-    Hand.new(hand).rank
   end
 end
 
