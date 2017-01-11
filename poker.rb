12ef6855944fce157bb9cf6a4bbaa0b4f198793a
Hemal <h.n.varambhia@gmail.com>
2017-01-11 15:12:13 +0000

    Refactor - introduced a Hand class that computes its own rank (made the implicit explicit).

--- poker.rb
+++ poker.rb
@@ -1,25 +1,40 @@
 class Poker
   attr_reader :hands
   def initialize hands
-    @hands = hands
+    @hands = hands.map { |hand| Hand.new hand }
   end
 
   def best_hand
-    [ hands.max_by { |hand| rank hand } ]
+    [ hands.max_by { |hand| hand.rank }.cards ]
   end
   
   private
 
+  class Hand
+    attr_reader :cards
+
+    def initialize cards
+      @cards = cards
+    end
+
+    def rank
+      ranks = cards.map { |card| card_rank card }
+        
+      ranks.map { |card| RANKS.index card }.max
+    end
+
+    private
+
+    def card_rank card
+      card[/\d+|[JQKA]/]
+    end
+
+    RANKS = %w{2 3 4 5 6 7 8 9 10 J Q K A} 
+  end
+
   def rank hand
-    ranks = hand.map { |card| card_rank card }
-    ranks.map { |card| RANKS.index card }.max
+    Hand.new(hand).rank
   end
-
-  def card_rank card
-    card[/\d+|[JQKA]/]
-  end
-
-  RANKS = %w{2 3 4 5 6 7 8 9 10 J Q K A} 
 end
 
 module BookKeeping
